9345d371597718e59fac7f955747b6c9b1dfc46b
Ahmet Tufan <gs_db_1905@hotmail.com>
2022-05-04 16:32:02 +0300

    güncel

--- app/src/main/java/com/ahmet/kotlinanimalapp/ui/bilgi/BilgiFragment.kt
+++ app/src/main/java/com/ahmet/kotlinanimalapp/ui/bilgi/BilgiFragment.kt
@@ -2,12 +2,15 @@ package com.ahmet.kotlinanimalapp.ui.bilgi
 
 import android.content.Intent
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.ahmet.kotlinanimalapp.databinding.FragmentBilgiBinding
+import kotlinx.android.synthetic.main.fragment_bilgi.*
 
 class BilgiFragment : Fragment() {
 
@@ -20,29 +23,45 @@ class BilgiFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val bilgiViewModel= ViewModelProvider(this).get(BilgiViewModel::class.java)
-
-        _binding= FragmentBilgiBinding.inflate(inflater,container,false)
-        val root:View=binding.root
 
 
-        bilgiViewModel.text.observe(viewLifecycleOwner) {
+
+
+            val bilgiViewModel = ViewModelProvider(this).get(BilgiViewModel::class.java)
+
+            _binding = FragmentBilgiBinding.inflate(inflater, container, false)
+            val root: View = binding.root
+
+            bilgiViewModel.text.observe(viewLifecycleOwner) {
+
+            }
+            return root
 
         }
-        return root
-
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+      /*
+        btnExpand.setOnClickListener {
+            if (expandableLayout.visibility == View.VISIBLE) {
+                TransitionManager.beginDelayedTransition(cardview, AutoTransition())
+                expandableLayout.visibility = View.VISIBLE
+                btnExpand.text = "KAPAT"
+            } else {
+                TransitionManager.beginDelayedTransition(cardview, AutoTransition())
+                expandableLayout.visibility = View.GONE
+                btnExpand.text = "OKU"
+            }
+        }
 
+       */
     }
 
 
+        override fun onDestroy() {
+            super.onDestroy()
+            _binding = null
+        }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding=null
-    }
 
 }
